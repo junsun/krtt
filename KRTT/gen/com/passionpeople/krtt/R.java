3c059c423ab53b2f2cda9b2dd607c04fd2a0b634
Administrator <Administrator@ebaek-PC>
2014-08-12 01:47:39 +0900

    Httpget까지 완성

--- KRTT/gen/com/passionpeople/krtt/R.java
+++ KRTT/gen/com/passionpeople/krtt/R.java
@@ -30,7 +30,8 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
-        public static final int ic_menu=0x7f020001;
+        public static final int ic_logo=0x7f020001;
+        public static final int ic_menu=0x7f020002;
     }
     public static final class id {
         public static final int BtnSlide=0x7f090002;
@@ -50,14 +51,24 @@ public final class R {
         public static final int listview_item_business=0x7f030003;
         public static final int listview_item_horizontal_shadow=0x7f030004;
         public static final int listview_item_vertical_shadow=0x7f030005;
+        public static final int menu_item_horizontal_r_shadow=0x7f030006;
+        public static final int menu_item_horizontal_shadow=0x7f030007;
     }
     public static final class menu {
         public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f060002;
+        public static final int action_settings=0x7f06000a;
         public static final int footer_add_bs=0x7f060001;
-        public static final int hello_world=0x7f060003;
+        public static final int hello_world=0x7f06000b;
+        public static final int menu_bs_etc=0x7f060009;
+        public static final int menu_bs_jeans=0x7f060007;
+        public static final int menu_bs_list=0x7f060002;
+        public static final int menu_bs_outers=0x7f060003;
+        public static final int menu_bs_pants=0x7f060008;
+        public static final int menu_bs_shirts=0x7f060005;
+        public static final int menu_bs_sweaters=0x7f060006;
+        public static final int menu_bs_t_shirts=0x7f060004;
         public static final int title_bar=0x7f060000;
     }
     public static final class style {
--- KRTT/src/com/passionpeople/krtt/MainActivity.java
+++ KRTT/src/com/passionpeople/krtt/MainActivity.java
@@ -1,26 +1,43 @@
 package com.passionpeople.krtt;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Date;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+
 import android.app.Activity;
 import android.content.Context;
-import android.graphics.Color;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.view.Window;
-import android.view.View.OnClickListener;
 import android.view.animation.Animation;
-import android.view.animation.TranslateAnimation;
 import android.view.animation.Animation.AnimationListener;
+import android.view.animation.TranslateAnimation;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
+
 
 import com.passionpeople.krtt.vo.Business;
 
@@ -31,7 +48,7 @@ public class MainActivity extends Activity implements AnimationListener {
 	private View listFooter;
 	DataAdapter adapter;
 	ArrayList<Business> alist;
-	View menu;
+	View menu; 
 	View app;
 	boolean menuOut = false;
 	AnimParams animParams = new AnimParams();
@@ -55,49 +72,33 @@ public class MainActivity extends Activity implements AnimationListener {
 		listview.setAdapter(adapter);
 
 		// Header, Footer 积己 棺 殿废
-		listHeader = getLayoutInflater().inflate(
-				R.layout.listview_header_business, null, false);
-		listFooter = getLayoutInflater().inflate(
-				R.layout.listview_footer_business, null, false);
+		listHeader = getLayoutInflater().inflate(R.layout.listview_header_business, null, false);
+		listFooter = getLayoutInflater().inflate(R.layout.listview_footer_business, null, false);
 
 		listview.addHeaderView(listHeader);
 		listview.addFooterView(listFooter);
 
-		adapter.add(new Business(getApplicationContext(), "模备1",
-				"www.areanashop.com"));
-		adapter.add(new Business(getApplicationContext(), "模备2",
-				"www.naver.com"));
-		adapter.add(new Business(getApplicationContext(), "模备3",
-				"www.peace-worker.com"));
-		adapter.add(new Business(getApplicationContext(), "模备4",
-				"www.google.com"));
-		adapter.add(new Business(getApplicationContext(), "模备1",
-				"www.areanashop.com"));
-		adapter.add(new Business(getApplicationContext(), "模备2",
-				"www.naver.com"));
-		adapter.add(new Business(getApplicationContext(), "模备3",
-				"www.peace-worker.com"));
-		adapter.add(new Business(getApplicationContext(), "模备4",
-				"www.google.com"));
-		adapter.add(new Business(getApplicationContext(), "模备1",
-				"www.areanashop.com"));
-		adapter.add(new Business(getApplicationContext(), "模备2",
-				"www.naver.com"));
-		adapter.add(new Business(getApplicationContext(), "模备3",
-				"www.peace-worker.com"));
-		adapter.add(new Business(getApplicationContext(), "模备4",
-				"www.google.com"));
-		adapter.add(new Business(getApplicationContext(), "模备1",
-				"www.areanashop.com"));
-		adapter.add(new Business(getApplicationContext(), "模备2",
-				"www.naver.com"));
-		adapter.add(new Business(getApplicationContext(), "模备3",
-				"www.peace-worker.com"));
+		adapter.add(new Business(getApplicationContext(), "模备1", "www.areanashop.com"));
+		adapter.add(new Business(getApplicationContext(), "模备2", "www.naver.com"));
+		adapter.add(new Business(getApplicationContext(), "模备3", "www.peace-worker.com"));
+		adapter.add(new Business(getApplicationContext(), "模备4", "www.google.com"));
+		adapter.add(new Business(getApplicationContext(), "模备1", "www.areanashop.com"));
+		adapter.add(new Business(getApplicationContext(), "模备2", "www.naver.com"));
+		adapter.add(new Business(getApplicationContext(), "模备3", "www.peace-worker.com"));
+		adapter.add(new Business(getApplicationContext(), "模备4", "www.google.com"));
+		adapter.add(new Business(getApplicationContext(), "模备1", "www.areanashop.com"));
+		adapter.add(new Business(getApplicationContext(), "模备2", "www.naver.com"));
+		adapter.add(new Business(getApplicationContext(), "模备3", "www.peace-worker.com"));
+		adapter.add(new Business(getApplicationContext(), "模备4", "www.google.com"));
+		adapter.add(new Business(getApplicationContext(), "模备1", "www.areanashop.com"));
+		adapter.add(new Business(getApplicationContext(), "模备2", "www.naver.com"));
+		adapter.add(new Business(getApplicationContext(), "模备3", "www.peace-worker.com"));
 		adapter.add(new Business(getApplicationContext(), "模备4",
 				"www.google.com"));
 	}
 
-	private class DataAdapter extends ArrayAdapter<Business> {
+    
+	private class DataAdapter extends ArrayAdapter<Business>{
 		private LayoutInflater mInflater;
 
 		public DataAdapter(Context context, ArrayList<Business> object) {
@@ -114,7 +115,7 @@ public class MainActivity extends Activity implements AnimationListener {
 			} else {
 				view = v;
 			}
-
+			
 			final Business data = this.getItem(position);
 			if (data != null) {
 				TextView tv = (TextView) view.findViewById(R.id.textView1);
@@ -155,6 +156,9 @@ public class MainActivity extends Activity implements AnimationListener {
 			menu.setVisibility(View.INVISIBLE);
 		}
 		layoutApp(menuOut);
+		
+		Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));
+		startActivity(intent);
 	}
 
 	@Override
@@ -178,6 +182,7 @@ public class MainActivity extends Activity implements AnimationListener {
 		}
 	}
 
+	
 	class ClickListener implements OnClickListener {
 		@Override
 		public void onClick(View v) {
@@ -191,38 +196,44 @@ public class MainActivity extends Activity implements AnimationListener {
 			int left = (int) (app.getMeasuredWidth() * 0.3);
 
 			if (!menuOut) {
-				// anim = AnimationUtils.loadAnimation(context,
-				// R.anim.push_right_out_80);
 				anim = new TranslateAnimation(0, left, 0, 0);
 				menu.setVisibility(View.VISIBLE);
 				animParams.init(left, 0, left + w, h);
 			} else {
-				// anim = AnimationUtils.loadAnimation(context,
-				// R.anim.push_left_in_80);
 				anim = new TranslateAnimation(0, -left, 0, 0);
 				animParams.init(0, 0, w, h);
 			}
 
 			anim.setDuration(400);
 			anim.setAnimationListener(me);
-			// Tell the animation to stay as it ended (we are going to set the
-			// app.layout first than remove this property)
 			anim.setFillAfter(true);
-
-			// Only use fillEnabled and fillAfter if we don't call layout
-			// ourselves.
-			// We need to do the layout ourselves and not use fillEnabled and
-			// fillAfter because when the anim is finished
-			// although the View appears to have moved, it is actually just a
-			// drawing effect and the View hasn't moved.
-			// Therefore clicking on the screen where the button appears does
-			// not work, but clicking where the View *was* does
-			// work.
-			// anim.setFillEnabled(true);
-			// anim.setFillAfter(true);
-
 			app.startAnimation(anim);
+			
+			HttpGetThread th = new HttpGetThread();
+			th.start();
 		}
 	}
 
+	
+	  // Thread 努贰胶
+    class HttpGetThread extends Thread implements Runnable {
+        @Override
+        public void run() {
+    		super.run();
+ 			try{
+		 		HttpClient client = new DefaultHttpClient();
+	    		String url = "http://54.191.253.51/";
+		        HttpGet get = new HttpGet(url);
+		        HttpResponse response = client.execute(get);
+		        HttpEntity resEntity = response.getEntity();
+		        
+	        	if(resEntity != null){
+		        	String res = EntityUtils.toString(resEntity);
+				 	System.out.println(res);
+				}
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+        }
+    }
 }
